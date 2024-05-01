2527708f58db4a92e79ee5d2c7f1fec399cb5bab
fwijaya918 <fwijaya918@gmail.com>
2024-05-01 19:38:50 +0700

    Update postRouter.js
    tambah ngelike

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -40,6 +40,80 @@ postRouter.post("/tambahpost", async (req, res) => {
     });
   }
 });
+postRouter.post("/like", async (req, res) => {
+  try {
+    let { post_id, username } = req.body;
+    let id = "LIKE_" + post_id + "_" + username;
+    const like = await db.Like.create({
+      id,
+      post_id,
+      username,
+    });
+    res.status(201).json({
+      status: "success",
+      data: like,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+postRouter.delete("/unlike", async (req, res) => {
+  try {
+    let { id } = req.body;
+    const like = await db.Like.findOne({
+      where: {
+        id,
+      },
+    });
+    console.log(like);
+    if (like == null) {
+      res.status(400).json({
+        status: "error",
+        message: "Like not found",
+      });
+      return;
+    } else {
+      await db.Like.destroy({
+        where: {
+          id,
+          //username
+        },
+      });
+      res.status(200).json({
+        status: "success",
+        message: "Unlike success",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+//bikin api buat ngambil semua like dari post_id
+postRouter.get("/like/:post_id", async (req, res) => {
+  try {
+    let { post_id } = req.params;
+    const likes = await db.Like.findAll({
+      where: {
+        post_id,
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      data: likes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
 
 function formatDate(date) {
   // Get the year, month, day, hours, minutes, and seconds
